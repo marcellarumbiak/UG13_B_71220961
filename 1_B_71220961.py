f0472b65fdae8beea05a42bfb5ad64b657d95466
marcellarumbiak <117884708+marcellarumbiak@users.noreply.github.com>
2022-12-15 12:55:07 +0700

    Add files via upload

---
+++ 1_B_71220961.py
@@ -0,0 +1,16 @@
+import turtle
+t=turtle.Turtle()
+t.penup()
+# draw letter M
+t.goto(-30,50) # centering in the screen
+t.pendown()
+t.pensize(10)
+t.pencolor("red")
+
+t.right(90)
+t.forward(150)
+
+t.goto(-30,50)
+t.goto(20,-20)
+t.goto(65,50)
+t.goto(65,-100)
